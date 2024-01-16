073dabbef954a0f785ed17864d6aa7e1723adf74
Ivan <writtenbyme@mail.ru>
2024-01-16 19:37:49 +0500

    Добавьте файлы проекта.

---
+++ Pract6_pt1_Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pract6_pt2_Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+            if (str == str2)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
